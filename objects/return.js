605774cdf6f7834658b9ef5c4e5f498af8dd56d2
Italo Castillo <italogitdoe@gmail.com>
2024-03-10 17:08:19 +0000

    Add  returnS

---
+++ objects/return.js
@@ -0,0 +1,13 @@
+function max(a, b){
+    return a > b ? a : b; 
+}
+
+function saludo(){
+    console.log('hola !!');
+}
+
+const M = max(33, 5);
+console.log(M); 
+
+const res = saludo();
+console.log(res);
