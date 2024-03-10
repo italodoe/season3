cd541e8cb683844115191d6e0d48a330bf2d6dcf
Italo Castillo <italogitdoe@gmail.com>
2024-03-10 17:17:36 +0000

    default with args

---
+++ objects/default-args.js
@@ -0,0 +1,11 @@
+function exclama(mensaje, numExclam = 0){
+    console.log(mensaje + "!".repeat(numExclam));
+}
+
+exclama('You only live once', 3);
+exclama('Only with the muse of life', 2);
+exclama('Life\'s journey on earth', 1);
+exclama('Only to be born and, to die one day.');
+
+
+
